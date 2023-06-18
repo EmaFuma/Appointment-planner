ebf1fdb26f1c10d7eac7871e18f737b36d2b70a7
Ema <fuma.e@hotmail.it>
2023-06-18 09:28:30 +0100

    Push file on GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Routes, Route, Navigate, NavLink } from "react-router-dom";
+import { AppointmentsPage } from "./containers/appointmentsPage";
+import { ContactsPage } from "./containers/contactsPage";
 
 function App() {
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+      </nav>
+      <main>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Navigate to={ROUTES.CONTACTS} />}
+          ></Route>
+          <Route
+            path={ROUTES.CONTACTS}
+            element={
+              <ContactsPage contacts={contacts} addContact={addContact} />
+            }
+          ></Route>
+          <Route
+            path={ROUTES.APPOINTMENTS}
+            element={
+              <AppointmentsPage
+                appointments={appointments}
+                addAppointment={addAppointment}
+                contacts={contacts}
+              />
+            }
+          ></Route>
+        </Routes>
+      </main>
+    </>
   );
 }
 
 export default App;
+
+// Since <Route></Route> is the only component that's able to be child of <Routes></Routes>
+// you have to write the place where you want to connect inside an element prop, inside the <Route></Route> component.
+// Look inside main section inside Route comonents
